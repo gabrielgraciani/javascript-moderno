cc8b7cab5a417903bf2ee3c15c22d1979d30da8c
Gabriel <gabrieltgraciani@hotmail.com>
2019-11-17 21:17:26 -0300

    Fim capítulo 11 - react

--- 11-react/exercicios/src/index.js
+++ 11-react/exercicios/src/index.js
@@ -10,10 +10,27 @@ import ReactDOM from 'react-dom';
 //ReactDOM.render(<BomDia nome="guilherme" />, document.getElementById('root'))
 
 //multiplos.jsx
-import {BoaTarde, BoaNoite} from "./componentes/Multiplos";
-
-ReactDOM.render(<div>
+//import {BoaTarde, BoaNoite} from "./componentes/Multiplos";
+/*ReactDOM.render(<div>
 	<BoaTarde nome="ana" />
 	<BoaNoite nome="bia"/>
 	</div>, document.getElementById('root'))
+ */
 
+
+//saudacao
+/*import Saudacao from './componentes/Saudacao';
+ReactDOM.render(
+	<div>
+		<Saudacao tipo="bom dia" nome="João" />
+	</div>
+, document.getElementById('root'));
+*/
+
+//pai e filho
+import Pai from './componentes/Pai';
+ReactDOM.render(
+	<div>
+		<Pai nome="paulo" sobrenome="silva" />
+	</div>
+, document.getElementById('root'));
